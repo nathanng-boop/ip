081d688437f49e34a19ebd817f403dd2a0114726
ZhiHou <ngzhihou@gmail.com>
2026-08-23 21:49:28 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/Nate.java
@@ -0,0 +1,15 @@
+public class Nate {
+    public static void main(String[] args) {
+        printLine();
+        System.out.println("Hello! I'm Nate.");
+        System.out.println("What can I do for you?");
+
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    private static void printLine(){
+        System.out.println("________________________________________");
+    }
+}
