6ae7bd64bebf2d3d9177caa8061655c6068c5291
ZhiHou <ngzhihou@gmail.com>
2026-08-27 01:32:28 +0800

    Add support for marking and unmarking tasks

--- src/main/java/Nate.java
+++ src/main/java/Nate.java
@@ -11,33 +11,50 @@ public class Nate {
         String input;
 
         int countOfTasks = 0;
-        String[] listOfTasks = new String[100];
+        Task[] listOfTasks = new Task[100];
 
         while (true) {
             input = in.nextLine();
             printLine();
 
-            if (input.equals("bye")){
+            if (input.equals("bye")) {
                 System.out.println("Byebye. Hope to see you soon!");
                 printLine();
                 break;
             } else if (input.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < countOfTasks; i++) {
-                    System.out.println(i + 1 + ". " + listOfTasks[i]);
+                    System.out.println((i + 1) + "." + listOfTasks[i].getTaskLine());
+                }
+                printLine();
+            } else if (input.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(input.substring(5)) - 1;
+                if (taskIndex >= 0 && taskIndex < countOfTasks) {
+                    listOfTasks[taskIndex].markAsDone();
+                    System.out.println("Good job! I've marked this task as done:");
+                    System.out.println("  " + listOfTasks[taskIndex].getTaskLine());
+                }
+                printLine();
+            } else if (input.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(input.substring(7)) - 1;
+                if (taskIndex >= 0 && taskIndex < countOfTasks) {
+                    listOfTasks[taskIndex].markAsNotDone();
+                    System.out.println("Okay, I've marked this task as not done yet:");
+                    System.out.println("  " + listOfTasks[taskIndex].getTaskLine());
                 }
                 printLine();
             } else {
+                listOfTasks[countOfTasks] = new Task(input);
+                countOfTasks++;
                 System.out.println("added: " + input);
                 printLine();
-                listOfTasks[countOfTasks] = input;
-                countOfTasks++;
             }
         }
 
         in.close();
     }
 
-    private static void printLine(){
+    private static void printLine() {
         System.out.println("________________________________________");
     }
 }
