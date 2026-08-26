6ad19675d3f1579208b83e76b85038c77b225f6a
ZhiHou <ngzhihou@gmail.com>
2026-08-26 19:20:01 +0800

    Add support for echo

--- src/main/java/Nate.java
+++ src/main/java/Nate.java
@@ -1,12 +1,29 @@
+import java.util.Scanner;
+
 public class Nate {
     public static void main(String[] args) {
         printLine();
         System.out.println("Hello! I'm Nate.");
         System.out.println("What can I do for you?");
+        printLine();
 
-        printLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        printLine();
+        Scanner in = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = in.nextLine();
+            printLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Byebye. Hope to see you soon!");
+                printLine();
+                break;
+            } else {
+                System.out.println(input);
+                printLine();
+            }
+        }
+        in.close();
     }
 
     private static void printLine(){
